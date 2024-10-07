c5e79401d31ce7a2b60114f1f1b88febdc7fc900
Fergato <167968808+Fergato@users.noreply.github.com>
2024-10-07 14:55:16 +0200

    Final push

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -30,53 +30,21 @@ function envoiFormulaire() {
         if (res.status === 200) {
           return res.json();
         } else if (res.status !== 200) {
-          // On vérifie s'il existe déjà un élément d'erreur
-          let existingErrorTag = document.querySelector(".error");
-          if (!existingErrorTag) {
-            // On crée un nouvel élément d'erreur uniquement s'il n'existe pas déjà
-            let errorTag = document.createElement("p");
-            errorTag.innerText = "Erreur dans l’identifiant ou le mot de passe";
-            formLogin.appendChild(errorTag);
-            errorTag.classList.add("error");
-          }
+          alert("Erreur dans l’identifiant ou le mot de passe");
+          throw new Error("Identifiants invalides");
         }
       })
       .then((data) => {
         const token = data.token;
         // Stocker le token dans le localStorage
         window.localStorage.setItem("token", JSON.stringify(token));
-        console.log(window.localStorage.getItem("token"));
-
         window.location.href = "index.html";
       })
-      .catch((error) =>
-        console.error("erreur lors de la récupération des données")
-      );
+      .catch((error) => {
+        console.error(error); // Gérer les erreurs ici
+      });
+    zoneEmail.value = "";
+    zoneMdp.value = "";
   });
-  //   try {
-  //     // Envoi du formulaire au format JSON
-  //     const response = await fetch("http://localhost:5678/api/users/login", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: chargeUtile,
-  //     });
-
-  //     if (response.ok) {
-  //       // Récupération de la réponse de l'API
-  //       const reponseJson = await response.json();
-  //       const token = JSON.stringify(reponseJson.token);
-  //       window.localStorage.setItem("token", token);
-  //       if ((window.location = "login.html")) {
-  //         window.location.replace("index.html");
-  //       }
-  //     } else {
-  //       alert("Erreur dans l’identifiant ou le mot de passe");
-  //       zoneEmail.value = null;
-  //       zoneMdp.value = null;
-  //     }
-  //   } catch (error) {
-  //     console.log("request failed");
-  //   }
-  // });
 }
 envoiFormulaire();
